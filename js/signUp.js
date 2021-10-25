446ba84e2ad4e8101271462806b3171e6994f65c
Cosmin <cosminmorari99@yahoo.com>
2021-10-25 18:38:57 +0300

    modificari

--- js/signUp.js
+++ js/signUp.js
@@ -1,29 +1,101 @@
-function validatorForm(){
-    let box1 = document.getElementById("box1");
-    let box2 = document.getElementById("box2");
-    let box3 = document.getElementById("box3");
-    let box4 = document.getElementById("box4");
-    let box5 = document.getElementById("box5");
-    let box6 = document.getElementById("box6");
-    let spChars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+const form = document.getElementById("form");
+const fname = document.getElementById("fname");
+const lname = document.getElementById("lname");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+//aic va trebui sa fac asemenea si pt celelalte campuri pe care le voi pune
 
-    //mai jos avem cod care impune completarea tuturor inputurilor
-    if(box1.value == "" || box2.value == "" || box3.value == ""  || box4.value == ""  || box5.value == "" || box6.value == "" ){
-        alert("Toate campurile sunt obligatorii!");   
-        return false;
-}
-    //mai jos avel cod care impune completarea NUMELUI si a PRENUMELUI cu minim 3 litere
-    else if(box1.value.length < 3 || box2.value.length < 3){
-        alert("Numele si prenumele trebuie sa contina minim 3 caractere!");
-        return false;
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs(){
+    //get the values from the inputs
+    const fnameValue = fname.value.trim();
+    const lnameValue = lname.value.trim();
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    
+        if (fnameValue ===''){
+            setErrorFor(fname, 'Required field.');
+        }        
+        else if(fnameValue.length < 3){
+            setErrorFor(fname, 'First must contain at least 3 letters');
+        }       
+        else {
+            setSuccesFor(fname);
+        }
+
+        if (lnameValue ===''){
+            setErrorFor(lname, 'Required field.');
+        }        
+        else if(lnameValue.length < 3){
+            setErrorFor(lname, 'First must contain at least 3 letters');
+        }       
+        else {
+            setSuccesFor(lname);
+        }
+
+
+        if(usernameValue === '') {
+            setErrorFor(username, 'Required field.');
+        }        
+        else if(usernameValue.length < 3){
+            setErrorFor(username, 'Username must contain at least 3 letters');
+        }       
+        else {
+            setSuccesFor(username);
+        }
+        if (emailValue === '') {
+            setErrorFor(email, 'Required field.');
+        } else if(!isEmail(emailValue)) {
+            setErrorFor(email, 'Email is not valid')
+        } else{
+            setSuccesFor(email);
+        }
+        if (passwordValue === '') {
+            setErrorFor(password, 'Required field.');
+        } else if(!isPassword(passwordValue)) {
+            setErrorFor(password, 'Password is not valid')}
+        else if(passwordValue.length < 8 ){
+            setErrorFor(password, 'The password must contain at least 8 characters, a capital letter and a number.');
+        }
+        else {
+            setSuccesFor(password);
+        }
+        if (password2Value === '') {
+            setErrorFor(password2, 'Required field.');
+    }   else if(passwordValue != password2Value) {
+            setErrorFor(password2, 'Passwords does not match')
+    }   else {
+            setSuccesFor(password2);
+    }
     }
 
-    //mai jos avem cod pentru a impune completarea parolei astfel: sa contina minim o litera mare, o cifra si un caracter special si sa aiba cel putin 8 caractere si maximul de 15 caractere!
-    else if(box4.value.length < 8 || box4.value.length > 15 || spChars.test()) {
-        alert("Parola trebuie sa contina minim 8 caractere si maxim 15 caractere!");
-    }
-   
+function setErrorFor(input, message){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+    formControl.className = 'form-control error';
 }
 
-//pana aici am facut validarea pentru cele 6 campuri ca fiind obligatorii, iar NUMELE si PRENUMELE SA AIBA MINIM 3 CARACTERE;
-//in continuare vom face validare pentru parola (sa contina minim: o litera mare, o cifra si un caracter special si sa aiba cel putin 8 caractere);
+function setSuccesFor(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control succes';
+}
+
+function isEmail(email){
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+function isPassword(password){
+    return /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(password);
+}
