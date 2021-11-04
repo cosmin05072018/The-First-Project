0c57270a7afadf7bb59d5f007ffa11264de10ba5
Cosmin <cosminmorari99@yahoo.com>
2021-11-04 19:13:02 +0200

    modificari din nou

--- js/forms.js
+++ js/forms.js
@@ -10,13 +10,16 @@ const tac = document.querySelector('#terms-and-cond') || null;
 const notification = document.querySelector('#notifications') || null;
 
 submitBtn.addEventListener('click', () => {
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
 if(name != null){  //sign up page
     if(name.value.length < 3){
         showAlert('name must be 3 letters long');
-    } else if(!email.value.length){
-        showAlert('enter you email');
-    } else if(password.value.length < 8){
-        showAlert('password  should be 8 letters long');
+    } else if(emailValue && !isEmail(emailValue)){
+        showAlert('Email is not valid');
+    } else if(password.value.length < 8 || !isPassword(passwordValue)){
+        showAlert('Password is not valid');
     } else if(!number.value.length){
         showAlert('enter your phone number');
     } else if(!Number(number.value) || number.value.length < 10){
@@ -30,6 +33,15 @@ if(name != null){  //sign up page
     }
 } 
 })
+
+//validari email si parola
+function isEmail(email){
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+function isPassword(password){
+    return /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(password);
+}
+
 
 //alert function 
 
