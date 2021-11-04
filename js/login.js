a1bd3c5fec7f141b2e5af80d8c2298cab70abd39
Cosmin <cosminmorari99@yahoo.com>
2021-11-04 19:33:15 +0200

    validari la pagina de login

--- js/login.js
+++ js/login.js
@@ -6,14 +6,29 @@ const email = document.querySelector('#email');
 
 
 submitBtn.addEventListener('click', () => {
-if(!email.value.length || !password.value.length){
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if(!email.value.length || !password.value.length ){
     showAlert('fill all the inputs');
-} else{
+    }  else if(emailValue && !isEmail(emailValue)){
+        showAlert('Email is not valid');
+    } else if(password.value.length < 8 || !isPassword(passwordValue)){
+        showAlert('Password is not valid');
+    }
+    else{
     loader.style.display = 'block';
     window.location="../html/homePage.html";
 }
 }
 )
+function isEmail(email){
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+function isPassword(password){
+    return /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(password);
+}
+
 
 const showAlert = (msg) => {
     let alertBox= document.querySelector('.alert-box');
