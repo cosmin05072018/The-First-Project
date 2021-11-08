744b8b1e959253dc5f329ce76ba914c9b1e05ef5
Cosmin <cosminmorari99@yahoo.com>
2021-11-08 22:23:38 +0200

    shoppingCart

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -7,73 +7,85 @@ let products = [
         collection: "women",
         name: "Rochie 'Gemma'",
         price: 90,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart1" 
     },
     {
         collection: "women",
         name: "Rochie 'Whitney'",
         price: 50,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart2" 
     },
     {
         collection: "women",
         name: "Rochie 'Sloane'",
         price: 30,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart3" 
     },
     {
         collection: "women",
         name: "Rochie 'ALIANNA'",
         price: 30,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart4" 
     },
     {
         collection: "women",
         name: "Rochie tip bluză 'SANIA'",
         price: 43,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart5" 
     },
     {
         collection: "women",
         name: "Rochie 'EIRA'",
         price: 33,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart6" 
     },
     {
         collection: "women",
         name: "Jeans",
         price: 20,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart7" 
     },
     {
         collection: "women",
         name: "Pantaloni 'FRANCIS'",
         price: 15,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart8" 
     },
     {
         collection: "women",
         name: "Jeans 'Mom 80'",
         price: 47,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart9" 
     },
     {
         collection: "women",
         name: "Bluză 'Kendra'",
         price: 44,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart10" 
     },
     {
         collection: "women",
         name: "Bluză 'Kendra'",
         price: 18,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart11" 
     },
     {
         collection: "women",
         name: "Tricou 'Katja'",
         price: 76,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart12"
     },
 
     //for men
@@ -82,147 +94,171 @@ let products = [
         collection: "men",
         name: "Jachetă fleece 'WILLI'",
         price: 20,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart13"
     },
     {
         collection: "men",
         name: "Tricou negru",
         price: 13,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart14" 
     },
     {
         collection: "men",
         name: "Tricou 'BORO'",
         price: 26,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart15" 
     },
     {
         collection: "men",
         name: "Jeans 'Ply'",
         price: 33,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart16" 
     },
     {
         collection: "men",
         name: "Cămașă 'JPRBLASUMMER'",
         price: 30,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart17" 
     },
     {
         collection: "men",
         name: "Camasa de vara",
         price: 18,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart18" 
     },
     {
         collection: "men",
         name: "Tricou 'Carl'",
         price: 15,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart19" 
     },
     {
         collection: "men",
         name: "Tricou Kan",
         price: 14,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart20" 
     },
     {
         collection: "men",
         name: "Hanorac 'JJEHOLMEN'",
         price: 20,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart21"  
     },
     {
         collection: "men",
         name: "Hanorac",
         price: 26,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart22"  
     },
     {
         collection: "men",
         name: "Camasa king",
         price: 14,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart23"  
     },
     {
         collection: "men",
         name: "Tricou black",
         price: 10,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart24"  
     },
 
     //for kids
     {
-        collection: "kids",
+        collection: "imbracaminteCopii",
         name: "Bluză de molton 'Black Cat'",
         price: 12,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart25"  
     },
     {
-        collection: "kids",
+        collection: "imbracaminteCopii",
         name: "Geacă de primăvară-toamnă",
         price: 39,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart26"  
     },
     {
-        collection: "kids",
+        collection: "imbracaminteCopii",
         name: "Hanorac",
         price: 22,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart27"  
     },
     {
-        collection: "kids",
+        collection: "imbracaminteCopii",
         name: "Hanorac 'Club'",
         price: 30,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart28"  
     },
     {
-        collection: "kids",
+        collection: "imbracaminteCopii",
         name: "Tricou 'Futura'",
         price: 43,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart29"  
     },
     {
-        collection: "kids",
+        collection: "imbracaminteCopii",
         name: "Tricou ' JUMBO FUTURA TEE'",
         price: 15,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart30"  
     },
     {
-        collection: "kids",
+        collection: "imbracaminteCopii",
         name: "Pantaloni nike",
         price: 14,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart31"   
     },
     {
-        collection: "kids",
+        collection: "imbracaminteCopii",
         name: "Pantaloni negri",
         price: 13,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart32"   
     },
     {
-        collection: "kids",
+        collection: "imbracaminteCopii",
         name: "Pantaloni gri",
         price: 18,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart33"   
     },
     {
-        collection: "kids",
+        collection: "imbracaminteCopii",
         name: "Pantaloni sport",
         price: 12,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart34"   
     },
     {
-        collection: "kids",
+        collection: "imbracaminteCopii",
         name: "Șorturi de baie",
         price: 10,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart35"   
     },
     {
-        collection: "kids",
+        collection: "imbracaminteCopii",
         name: "Tricou",
         price: 11,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart36"  
     },
 
 
@@ -231,73 +267,85 @@ let products = [
         collection: "accessories",
         name: "Ochelari de soare",
         price: 10,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart37"   
     },
     {
         collection: "accessories",
         name: "Rucsac",
         price: 14,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart38"   
     },
     {
         collection: "accessories",
         name: "Geantă",
         price: 16,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart39"   
     },
     {
         collection: "accessories",
         name: "Sneaker 'Gazelle'",
         price: 15,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart40"   
     },
     {
         collection: "accessories",
         name: "Geantă CALVIN KLEIN",
         price: 11,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart41"  
     },
     {
         collection: "accessories",
         name: "Geantă cK",
         price: 14,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart42"  
     },
     {
         collection: "accessories",
         name: "Fular gri",
         price: 18,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart43"  
     },
     {
         collection: "accessories",
         name: "Fular",
         price: 17,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart44"  
     },
     {
         collection: "accessories",
         name: "Căciuli sport 'Walden'",
         price: 24,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart45"  
     },
     {
         collection: "accessories",
         name: "Mănuși sport 'Grommitt Mittens'",
         price: 20,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart46"  
     },
     {
         collection: "accessories",
         name: "Căciulă",
         price: 16,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart47"  
     },
     {
         collection: "accessories",
         name: "Pălărie 'COTTON-CLSC CAP-AC-HAT'",
         price: 11,
-        inCart: 0 
+        inCart: 0,
+        imgSourceName: "cart48"  
     }
 ]
 
@@ -348,19 +396,15 @@ function setItems(product){
     cartItems = {
         [product.name]: product
     }
-    
     }
     
     localStorage.setItem("productsInCart", JSON.stringify(cartItems));
-    
 }
 
 function totalCost(product){
-    //console.log('produsul costa', product.price);
     
     let cartCost = localStorage.getItem('totalCost');
-    console.log('My cart cost is', cartCost);
-    console.log(typeof(cartCost));
+    
 
     if(cartCost != null){
         cartCost = parseInt(cartCost);
@@ -368,8 +412,29 @@ function totalCost(product){
     }else{
         localStorage.setItem("totalCost", product.price)
     }
+}
 
+function displayCart(){
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector('.products-container');
+    let cartCost = localStorage.getItem('totalCost');
 
+    if(cartItems && productContainer){
+       productContainer.innerHTML = '';
+       Object.values(cartItems).map(item =>{
+        productContainer.innerHTML += `
+            <div class="product">
+            <ion-icon name="close-circle-outline"></ion-icon>
+            <img src="../images/${item.collection}/${item.imgSourceName}.jpg">
+            <span>${item.name}</span>
+            </div>
+        `;
+        console.log(productContainer.innerHTML );
+       });
+
+    }
 }
 
 onLoadCartNumbers();
+displayCart();
