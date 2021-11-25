9c14c852524ed646db99d58fc38b1889dfaee22f
Lucian Sandu <dev.luciansandu@gmail.com>
2021-11-25 16:31:03 +0200

    added product details page for specific product

--- js/detailsProducts.js
+++
@@ -1,8 +0,0 @@
-let details = document.querySelectorAll('.detailsProduct');
-
-for (let i = 0; i < details.length; i++){
-    details[i].addEventListener('click', () => {
-        console.log('run');
-    })
-}
-
